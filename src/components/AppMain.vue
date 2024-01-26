61ce7d93ae3ce2cfa4782958d12bb07aac59d75c
StefanDellamore <dellamoreste.info@gmail.com>
2024-01-26 17:11:44 +0100

    Impostazioni delle flag

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -8,7 +8,19 @@ export default {
         };
     },
     methods: {
-
+        getFlag (lang) {
+            let linkFlag = '';
+            if (lang == 'en') {
+                linkFlag = 'https://flagicons.lipis.dev/flags/4x3/gb.svg';
+            }
+            else if (lang == 'ja') {
+                linkFlag = 'https://flagicons.lipis.dev/flags/4x3/jp.svg';
+            }
+            else {
+                linkFlag = 'https://flagicons.lipis.dev/flags/4x3/' + lang + '.svg' ;               
+            }
+            return linkFlag;
+        }
     }
 }
 </script>
@@ -20,13 +32,21 @@ export default {
               <li v-for="(movie, i) in store.movies" :key="i">
                  <div>Title: {{ movie.title }}</div>
                  <div>Original Title: {{ movie.original_title }}</div>
-                 <div>Original language: {{ movie.original_language }}</div>
-                 <div>vote avarage: {{ movie.vote_average }}</div>                                                                                                                       
+                 <div>
+                    flag:
+                    <img :src="getFlag(movie.original_language)" :alt="movie.original_language">
+                 </div>
+                 <div>vote avarage: {{ movie.vote_average }}</div>                                                                                                                      
               </li>
           </ul>
       </div>
+      
   </main>
 </template>
 
 <style lang="scss" scoped>
+
+img {
+    height: 25px;
+}
 </style>
